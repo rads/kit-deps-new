9eb77c16136a269a528ae31863caa49498ce55a4
Radford Smith <11401+rads@users.noreply.github.com>
2022-07-24 12:07:23 -0700

    Parse all selmer files

--- src/io/github/kit_clj/deps_new_template.clj
+++ src/io/github/kit_clj/deps_new_template.clj
@@ -19,21 +19,32 @@
        (take n)
        (apply str)))
 
+(def selmer-paths
+  ["deps.edn"
+   "src/clj/core.clj"
+   "src/clj/web/middleware/core.clj"
+   "resources/system.edn"])
+
 (defn data-fn [{:keys [template-dir target-dir] :as data}]
   (let [full-name (:name data)
         [_ name] (string/split full-name #"/")
         versions (edn/read-string (slurp (str template-dir "/versions.edn")))
-        deps-raw (slurp (str template-dir "/root/deps.edn"))
-        deps-path (fs/create-temp-file)
         selmer-opts (merge data
                            {:versions versions}
                            (update-keys data #(edn/read-string (str % "?"))))
-        deps-parsed (render-selmer deps-raw selmer-opts)]
+        selmer-map (->> selmer-paths
+                        (map (fn [path]
+                               (let [raw (slurp (str template-dir "/root/" path))]
+                                 [path {:file-path path
+                                        :parsed (render-selmer raw selmer-opts)
+                                        :temp-file (fs/create-temp-file)}])))
+                        (into {}))]
     (fs/delete-on-exit (fs/file (str target-dir "/gitignore")))
-    (fs/delete-on-exit deps-path)
-    (spit (fs/file deps-path) deps-parsed)
+    (doseq [[_ {:keys [temp-file parsed]}] selmer-map]
+      (fs/delete-on-exit temp-file)
+      (spit (fs/file temp-file) parsed))
     (-> data
-        (merge {:deps-path (str deps-path)
+        (merge {:selmer-map selmer-map
                 :full-name full-name
                 :app name
                 :ns-name (deps-new-impl/->ns full-name)
@@ -41,8 +52,12 @@
                 :sanitized (deps-new-impl/->file full-name)
                 :default-cookie-secret (rand-str 16)}))))
 
-(defn template-fn [template {:keys [deps-path template-dir target-dir]}]
+(defn template-fn [template {:keys [template-dir selmer-map]}]
   (let [extra-dir (str (fs/relativize template-dir (str (fs/temp-dir))))
-        extra-files [extra-dir {(fs/file-name deps-path) "deps.edn"} :only]]
+        rename-map (->> selmer-map
+                        (map (fn [[_ {:keys [temp-file file-path]}]]
+                               [(fs/file-name temp-file) file-path]))
+                        (into {}))
+        extra-files [extra-dir rename-map :only]]
     (-> template
         (update :transform conj extra-files))))
